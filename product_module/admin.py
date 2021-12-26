46a9d1d538500d047a2e1983a7a59b5efa94c4a0
faatemeh chamani <fch199674@yahoo.com>
2021-12-26 11:47:00 +0330

    product model  updated

--- product_module/admin.py
+++ product_module/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from . import models
 
 
-class ProductTabular( admin.TabularInline ):
+class ProductTabularDetail( admin.TabularInline ):
     model = models.ProductDetail
 
 
 class ProductAdmin( admin.ModelAdmin ):
-    inlines = [ProductTabular]
-    list_display = ('__str__', 'added_date', 'slug')
+    inlines = [ProductTabularDetail]
+    list_display = ('__str__', 'added_date', 'brand')
 
 
 class ProductDetailAdmin( admin.ModelAdmin ):
@@ -18,3 +18,4 @@ class ProductDetailAdmin( admin.ModelAdmin ):
 admin.site.register( models.Product, ProductAdmin )
 admin.site.register( models.ProductDetail, ProductDetailAdmin )
 admin.site.register( models.Category )
+admin.site.register( models.Brand )
--- product_module/models.py
+++ product_module/models.py
@@ -3,6 +3,17 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 
+class Brand( models.Model ):
+    title = models.CharField( max_length=200 )
+
+    class Meta:
+        verbose_name = 'Product Brand'
+        verbose_name_plural = 'Product Brands List'
+
+    def __str__(self):
+        return self.title
+
+
 class Category( models.Model ):
     title = models.CharField( max_length=200 )
 
@@ -46,6 +57,7 @@ class Product( models.Model ):
                                          db_index=True,
                                          null=True, blank=True )
     sell_count = models.IntegerField( default=0, null=True, blank=True, help_text='Number of product sales' )
+    brand = models.ForeignKey( Brand, on_delete=models.CASCADE, db_index=True, null=True )
 
     class Meta:
         verbose_name = 'Product'
