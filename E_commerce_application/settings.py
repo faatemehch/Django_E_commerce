76ba160a8f0db8a67b83b411498916ae4f9241a9
faatemeh chamani <fch199674@yahoo.com>
2021-12-26 11:09:56 +0330

    static files added

--- E_commerce_application/settings.py
+++ E_commerce_application/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = Path( __file__ ).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -26,7 +26,6 @@ SECRET_KEY = 'django-insecure-u#st2e4ivh1*=tr98l8@*$pj%gov0y98_kt+0kajuwo=1_9f8+
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'E_commerce_application.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -82,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -102,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -114,11 +110,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join( BASE_DIR, 'static' )]
+STATIC_ROOT = os.path.join( BASE_DIR, 'static_cdn', 'static_root' )
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join( BASE_DIR, 'static_cdn', 'media_root' )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- product_module/views.py
+++ product_module/views.py
@@ -7,7 +7,17 @@ class ProductListView( ListView ):
     model = Product
     template_name = 'product_module/product_list_page.html'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data( **kwargs )
+        context['title'] = 'product list'
+        return context
+
 
 class ProductDetailView( DetailView ):
     model = Product
     template_name = 'product_module/product_detail_view_page.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data( **kwargs )
+        context['title'] = 'product detail'
+        return context
