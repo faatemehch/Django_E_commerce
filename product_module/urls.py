3926a12bb88491e42f10831686d48d06cea149d4
faatemeh chamani <fch199674@yahoo.com>
2021-12-26 13:07:43 +0330

    views updated

--- product_module/urls.py
+++ product_module/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'product_module'
@@ -7,4 +7,5 @@ urlpatterns = [
     path( '', views.ProductListView.as_view(), name='product-list' ),
     path( 'product/<slug:slug>/', views.ProductDetailView.as_view(), name='product-detail' ),
     path( 'new-products/', views.NewProductListView.as_view(), name='new-product-list' ),
+    path( 'product-brand/<brand_name>/', views.ProductByBrand.as_view(), name='product-list-by-brand' ),
 ]
--- product_module/views.py
+++ product_module/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Product, ProductDetail, Category
+from .models import Product, ProductDetail, Category, Brand
 
 
 class ProductListView( ListView ):
@@ -11,6 +11,7 @@ class ProductListView( ListView ):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data( **kwargs )
         context['title'] = 'product list'
+        context['brands'] = Brand.objects.all()
         return context
 
 
@@ -18,9 +19,10 @@ class ProductDetailView( DetailView ):
     model = Product
     template_name = 'product_module/product_detail_view_page.html'
 
-    def get_context_data(self, *, object_list=None, **kwargs):
+    def get_context_data(self, *args, **kwargs):
         context = super().get_context_data( **kwargs )
         context['title'] = 'product detail'
+
         return context
 
 
@@ -36,4 +38,22 @@ class NewProductListView( ListView ):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data( **kwargs )
         context['title'] = 'new products'
+        context['brands'] = Brand.objects.all()
         return context
+
+
+class ProductByBrand( ListView ):
+    model = Product
+    template_name = 'product_module/product_list_page.html'
+    paginate_by = 6
+
+    def get_queryset(self, *, object_list=None, **kwargs):
+        brand_name = self.kwargs.get( 'brand_name' )
+        return Product.objects.filter( brand__title=brand_name ).order_by( '-added_date' )
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data( **kwargs )
+        context['title'] = 'products by brand'
+        context['brands'] = Brand.objects.all()
+        return context
+
