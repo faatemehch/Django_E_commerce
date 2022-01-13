2e80408458121cd92652755bf9768a6363c7273b
faatemeh chamani <fch199674@yahoo.com>
2022-01-13 16:33:17 +0330

    bug fixes

--- order_module/views.py
+++ order_module/views.py
@@ -22,8 +22,9 @@ def add_user_order(request):
     # find or create an open order
     order: Order = Order.objects.filter( owner=request.user, is_paid=False ).first()
     if order is None:
+        tracking_code = create_tracking_code()
         order: Order = Order.objects.create( owner=request.user,
-                                             tracking_code=f'{create_tracking_code}+{request.user.id}' )
+                                             tracking_code=tracking_code )
     order_detail: OrderDetail = order.orderdetail_set.filter( product=product, product_detail=product_detail ).first()
     if order_detail is None:
         order_detail = order.orderdetail_set.create( product=product, product_detail=product_detail,
