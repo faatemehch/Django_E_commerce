950b2a87ee6a22fcfe99a9a623d095327e62bed5
fatemeh chamani <fch199674@yahoo.com>
2022-01-16 11:47:53 +0330

    retrieving password

--- E_commerce_application/settings.py
+++ E_commerce_application/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path( __file__ ).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'account_module',
     'contact_module',
     'order_module',
-    'forgot_password_module'
+    'forgot_password_module.apps.ForgotPasswordModuleConfig'
 ]
 
 MIDDLEWARE = [
@@ -120,12 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join( BASE_DIR, 'static' )]
-STATIC_ROOT = os.path.join( BASE_DIR, 'static_cdn', 'static_root' )
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'static_root')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join( BASE_DIR, 'static_cdn', 'media_root' )
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'media_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
