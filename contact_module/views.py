78f03f896dea7aa770a2cde386515b91ec56b78b
fatemeh chamani <fch199674@yahoo.com>
2022-01-17 16:23:09 +0330

    views updated

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.edit import FormView
 from .forms import ContactUsForm
 
 
-class ContactUsView( FormView ):
+class ContactUsView(FormView):
     template_name = 'contact_module/contact_us_page.html'
     form_class = ContactUsForm
+    success_url = reverse_lazy('contact_module:contact-us')
 
     def form_valid(self, form):
         form.save()
-        return HttpResponseRedirect( self.request.path_info )
+        return super().form_valid(form)
